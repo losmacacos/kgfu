abd40e27c5fbb3aa5889cd8eff39ee9f091599dc
losmacacos <alexandrferreira@gmail.com>
2017-12-02 20:27:47 -0200

    third commit

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 try{
 	
 	$conn = new PDO("pgsql:host=ec2-184-73-174-10.compute-1.amazonaws.com port=5432 user=zwjaegfjfxxizf password=5049949664477c785135e52fbb54265edc71a7a3c2c67cfdcc878f16f7763741
- dbname=kungfuatibaia2");
+ dbname=d2jljet10m16fv");
 } catch(PDOException $e){
 	die( "Connection failed: " . $e->getMessage());
 }
