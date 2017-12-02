29b0f47f890fbd3aa97e32ca86052a0cc4bffbc2
losmacacos <alexandrferreira@gmail.com>
2017-12-02 20:21:49 -0200

    second commit

--- database.php
+++ database.php
@@ -2,7 +2,8 @@
 
 try{
 	
-	$conn = new PDO("pgsql:host=localhost port=5432 user=postgres password=123456 dbname=kungfuatibaia2");
+	$conn = new PDO("pgsql:host=ec2-184-73-174-10.compute-1.amazonaws.com port=5432 user=zwjaegfjfxxizf password=5049949664477c785135e52fbb54265edc71a7a3c2c67cfdcc878f16f7763741
+ dbname=kungfuatibaia2");
 } catch(PDOException $e){
 	die( "Connection failed: " . $e->getMessage());
 }
