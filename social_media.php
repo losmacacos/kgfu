0ea1c894698e4cd79c2a1a3b8eabbf96ec89b88f
losmacacos <alexandrferreira@gmail.com>
2017-12-04 21:57:58 -0200

    twelve commit

--- social_media.php
+++ social_media.php
@@ -95,7 +95,7 @@
 
                     <?php 
  
-                     $access_token = SEU-ACCESS-TOKEN;
+                     $access_token = 6663446711.1677ed0.0b4922aa447c47179b7fd71225509ae8;
                      $user = file_get_contents( "https://api.instagram.com/v1/users/self/media/recent/?access_token=$access_token" );
                      $user = json_decode($user, true)['data'];
                      ?>
