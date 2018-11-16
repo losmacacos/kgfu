9a27a5fcf6c9692ed41a8691a60acd1d6c9a74f6
losmacacos <losmacacos@gmail.com>
2018-11-16 11:08:12 -0200

    Update email.php

--- email.php
+++ email.php
@@ -17,7 +17,7 @@ function smtpmailer($para, $de, $de_nome, $assunto, $corpo) {
 	global $error;
 	$mail = new PHPMailer();
 	$mail->IsSMTP();		// Ativar SMTP
-	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPDebug = 1;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
 	$mail->SMTPAuth = true;		// Autenticação ativada
 	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
 	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
