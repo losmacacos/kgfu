a2c718c864c6ee9cabfaf1c49be3c7454fdcb0d6
losmacacos <losmacacos@gmail.com>
2018-11-15 22:30:02 -0200

    Update index.php

--- index.php
+++ index.php
@@ -703,15 +703,15 @@
              if ($_POST) 
              {
              //Carrega as classes do PHPMailer
-             include("../phpmailer/class.phpmailer.php"); 
-             include("../phpmailer/class.smtp.php"); 
+             include("phpmailer/class.phpmailer.php"); 
+             include("phpmailer/class.smtp.php"); 
              
              //envia o e-mail para o visitante do site
              $mailDestino = $_POST['contactEmail']; 
              $nome = $_POST['contactName']; 
              $mensagem = "Obrigado pelo seu contato, responderemos em breve!";
              $assunto = "Obrigado pelo seu contato!";
-             include("../envio.php");
+             include("envio.php");
              
              //envia o e-mail para o administrador do site
              $mailDestino = 'andressanardini@yahoo.com.br'; 
@@ -721,7 +721,7 @@
              <strong>Nome de quem enviou:</strong> $_POST[contactName]<br/>
              <strong>e-mail:</strong> $_POST[contactEmail]<br/>
              <strong>mensagem enviada:</strong> $_POST[contactMessage]";
-             include("../envio.php");
+             include("envio.php");
              }
             ?>
         <div class="row contact__main">
